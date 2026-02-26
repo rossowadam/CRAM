a9c4bca0c7b0d5f7f2ede92792c58fd1e37bb372
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-02-26 01:50:31 -0600

    chore: change validation returns to errors messages instead of boolean

--- frontend/src/utils/Validators.ts
+++ frontend/src/utils/Validators.ts
@@ -9,36 +9,36 @@ type SignupInput = {
 /*
 Validate the signup input by checking each attribute
 against the decided-upon requirements.
-Returns an errors object which may contain falsified attributed
+Returns an errors object which may contain error messages
 corresponding to the failed check(s).
 */
 export function validateSignup(input: SignupInput) {
     const { name, email, password, confirmPassword } = input;
     let errors: {
-        name?: boolean;
-        email?: boolean;
-        password?: boolean;
-        confirmPassword?: boolean;
+        name?: string;
+        email?: string;
+        password?: string;
+        confirmPassword?: string;
     } = {};
 
     // name to not be empty
     if (!name.trim()) {
-        errors.name = false;
+        errors.name = "Name field was empty";
     }
 
     // email domain may only be @myumanitoba or @umanitoba
     if (!email.endsWith("@myumanitoba.ca") && !email.endsWith("@umanitoba.ca")) {
-        errors.email = false;
+        errors.email = "Email domain is not valid";
     }
 
     // password must be at least 8 chars long
     if (password.length < 8) {
-        errors.password = false;
+        errors.password = "Password too short";
     }
 
     // password must match confirmPassword
     if (confirmPassword !== password) {
-        errors.confirmPassword = false;
+        errors.confirmPassword = "Passwords do not match";
     }
 
     // return empty object if valid
