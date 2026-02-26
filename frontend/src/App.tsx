6e1e7ad57bf14918421a04f825ef7c86c419367f
schwarzd-dev <schwarzd@myumanitoba.ca>
2026-02-25 20:58:01 -0600

    feat: add sidebar component
    - add tool tip provider and sidebar provider to app.tsx
    - create course sidebar component
    - install shadcn components
    - create course page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import {Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import Navbar from './components/layout/Navbar';
+import Course from './pages/Course';
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import { TooltipProvider } from './components/ui/tooltip';
+import CourseSidebar from './components/layout/CourseSidebar';
 
 function App(){
   return (
@@ -8,6 +12,24 @@ function App(){
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
+
+        <Route path='/course/:courseId' element={
+          <TooltipProvider>
+            <SidebarProvider 
+              defaultOpen={true}
+              style={
+                {
+                  "--sidebar-width": "20rem",
+                  "--sidebar-width-mobile": "20rem",
+                } as React.CSSProperties
+              }
+            >
+              <CourseSidebar />
+              <Course />
+              
+            </SidebarProvider>
+          </TooltipProvider>
+        }/>
       </Routes>     
     </>
   );
---
+++ frontend/src/pages/Course.tsx
@@ -0,0 +1,14 @@
+import { CollapsibleContent } from "@/components/ui/collapsible";
+import { SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Sidebar } from "lucide-react";
+import { useParams } from "react-router-dom";
+
+export default function Course() {
+  const { courseId } = useParams();
+    
+return (
+    <div className="flex flex-col w-full max-w-2xl  items-center gap-3 font-funnel sm:max-w-xl lg:max-w-2xl xl:max-w-4xl 2xl:max-w-5xl mx-auto px-6 py-6 ">
+        <h1>Course Title {courseId}</h1>
+    </div>
+  );
+}
