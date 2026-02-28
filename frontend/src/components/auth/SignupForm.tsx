00fc9a65bb6629b54347d4827e2e21be9170c844
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-02-27 22:56:46 -0600

    fix: remove temp name to validators to properly register capitalization change

--- frontend/src/components/auth/SignupForm.tsx
+++ frontend/src/components/auth/SignupForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 
-import { validateSignup } from "@/utils/tempValidators";
+import { validateSignup } from "@/utils/validators";
 interface SignupFormProps {
     setOpen: (open: boolean) => void;
 }
--- frontend/src/utils/validators.test.ts
+++ frontend/src/utils/validators.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { validateSignup } from "./tempValidators";
+import { validateSignup } from "./validators";
 
 describe("validateSignup", () => {
 
