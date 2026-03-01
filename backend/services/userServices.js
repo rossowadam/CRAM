25fc93e36875fb841ac0da8f216cb25d8eda2309
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-01 17:26:08 -0600

    refactor: redo createUser validation code to match new schema

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -13,35 +13,32 @@ exports.deleteUserById = async (id) => {
 
 //verifies that the user can create an account, and that no duplicate accounts exist with the same email, then creates a new user document in the database
 exports.createUser = async (userData) => {
+    const { name, email, password } = userData; // extract data
 
-    const userIsComplete = userData.id && userData.firstName && userData.lastName && userData.email && userData.passwordHash && userData.userName;
-    if (!userIsComplete) {
+    // incomplete if any field is missing
+    if (!name || !email || !password) {
         throw new Error('User data is incomplete');
     }
 
+    // domain not allowed check
     const allowedDomains = ['@umanitoba.ca', '@myumanitoba.ca'];
-    const {email} = userData;
-    
+
     const allowed = allowedDomains.some(domain => email.endsWith(domain));
 
-    if (!allowed) {
-        throw new Error('Email domain is not allowed');
-    }
-
+    if (!allowed) throw new Error('Email domain is not allowed');
     
-    checkForExistingUser = await userRepository.findUserByEmail(userData.email);
-    if (checkForExistingUser) {
-        throw new Error('User with this email already exists');
-    }
+    // check if user already exists
+    let doesExist = await userRepository.findUserByEmail(email);
+
+    if (doesExist) throw new Error('User with this email already exists');
     
-
-
+    // assign role
     if(email.endsWith('@myumanitoba.ca')) {
         userData.role = 'student';
     }
     else if(email.endsWith('@umanitoba.ca')) {
         userData.role = 'professor';
     }
-
-    return await userRepository.createUser(userData);
+    
+    //return await userRepository.createUser(userData);
 }
