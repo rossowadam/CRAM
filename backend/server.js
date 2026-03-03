e91bbc0473d7b09b51ebdef3d78ab2d90615327a
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-02 21:45:19 -0600

    chore: setup middleware logic to generate sessions collection for session IDs using connect-mongo

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 
 //Define routes
 const userRoutes = require('./routes/userRoutes');
@@ -30,6 +32,22 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// session middleware setup to generate session ID
+// store in Mongo and send cookies in response header
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI,
+        collectionName: "sessions"
+    }),
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 // 1 day
+    }
+}));
 
 //Send requests to routes, if the request is for /api/v1/courses, it will go to courseRoutes, if the request is for /api/v1/user, it will go to userRoutes
 app.use('/api/v1/courses', courseRoutes);
@@ -38,6 +56,7 @@ app.use('/api/v1/user', userRoutes);
 app.get('/', (req, res) => {
     res.send('CRAM API is running...');
 });
+
 
 // Define server port (defaults to 5000 if not specified in .env)
 const PORT = process.env.PORT || 5000;
