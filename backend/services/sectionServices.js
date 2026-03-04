38b3088e7768725221111781ad1679fc882489d7
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-04 15:21:58 -0600

    fix: wire up create section service to store based on section schema

--- backend/services/sectionServices.js
+++ backend/services/sectionServices.js
@@ -1,15 +1,26 @@
 const sectionRepository = require('../repositories/sectionRepository');
 
 exports.createSection = async (sectionData) => {
-    if (!sectionData.section_title || !sectionData.course_code || !sectionData.description || !sectionData.body) {
+    const { courseCode, title, subtitle, content } = sectionData; // extract data
+
+    if (!courseCode || !title || !subtitle || !content) {
         throw new Error('Section data is incomplete');
     }
-    const {section_title} = sectionData;
-    const existingSection = await sectionRepository.findSectionByTitle(section_title);
-    if (existingSection) {
-        throw new Error('Section with this title already exists');
-    }
-    return await sectionRepository.createSection(sectionData);
+    // TODO: findSectionByTitle is missing!
+    // const existingSection = await sectionRepository.findSectionByTitle(title);
+    // if (existingSection) {
+    //     throw new Error('Section with this title already exists');
+    // }
+
+     // create new section object to match Section schema
+    const newSection = {
+        course_code: courseCode,
+        title,
+        description: subtitle,
+        body: content
+    };
+
+    return await sectionRepository.createSection(newSection);
 }
 
 exports.getSectionsByCourseCode = async (courseCode) => {
