945aa8005cb0c0e92eed823a5d11b9b6859cf993
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-05 20:39:22 -0600

    fix: change duplicate section check to be a boolean instead of length check

--- backend/repositories/sectionRepository.js
+++ backend/repositories/sectionRepository.js
@@ -1,26 +1,30 @@
-const Section = require('../models/Section');
-
+const Section = require("../models/Section");
 
 exports.createSection = async (sectionData) => {
     const newSection = new Section(sectionData);
     await newSection.save();
     return newSection.toJSON();
-}
-
+};
 
 exports.getSectionsByCourseCode = async (courseCode) => {
     return await Section.find({ courseCode: courseCode }).lean();
-}
+};
 
 // return array of sections with the same code and title
-exports.findSectionByTitle = async ({courseCode, title}) => {
-    return await Section.find({ courseCode: courseCode, title: title }).lean();
-}
+exports.isDuplicateSection = async ({ courseCode, title }) => {
+    const existingSection = await Section.find({
+        courseCode: courseCode,
+        title: title,
+    }).lean();
+    return existingSection.length !== 0;
+};
 
 exports.deleteSection = async (id) => {
     return await Section.findByIdAndDelete(id);
-}
+};
 
 exports.updateSection = async (id, updateData) => {
-    return await Section.findByIdAndUpdate(id, updateData, { new: true }).lean();
-}
+    return await Section.findByIdAndUpdate(id, updateData, {
+        new: true,
+    }).lean();
+};
--- backend/tests/services/sectionServices.test.js
+++ backend/tests/services/sectionServices.test.js
@@ -3,10 +3,7 @@ const assert = require('node:assert/strict');
 const sectionRepository = require('../../repositories/sectionRepository');
 const sectionService = require('../../services/sectionServices');
 
-
-
-
-test('sectionService - createsection', async (t) => {
+test('sectionService - createSection', async (t) => {
     // Mock section data
     const sectionData = {    
         courseCode: 'TC101',
@@ -14,16 +11,16 @@ test('sectionService - createsection', async (t) => {
         description: 'derp',
         body: 'This is a test section'
     };
-    t.mock.method(sectionRepository, 'findSectionByTitle', async (courseCode, title) => {
-        return null; // Simulate no existing section with the same course code
+    t.mock.method(sectionRepository, 'isDuplicateSection', async (courseCode, title) => {
+        return false; // Simulate no existing section with the same course code
     });
     t.mock.method(sectionRepository, 'createSection', async (data) => {
         return data; // Simulate successful section creation
     }); 
-    const createdsection = await sectionService.createSection(sectionData);
-    assert.deepStrictEqual(createdsection, sectionData);
+    const createdSection = await sectionService.createSection(sectionData);
+    assert.deepStrictEqual(createdSection, sectionData);
 });
-test('sectionService - createsection with incomplete data', async (t) => {
+test('sectionService - createSection with incomplete data', async (t) => {
     // Mock incomplete section data
     const sectionData = {    
         id: 'section123',
@@ -39,7 +36,7 @@ test('sectionService - createsection with incomplete data', async (t) => {
     }   
 });
 
-test('sectionService - createsection with duplicate section', async (t) => {
+test('sectionService - createSection with duplicate section', async (t) => {
     // Mock section data
     const sectionData = {
         id: 'section123',
@@ -48,8 +45,8 @@ test('sectionService - createsection with duplicate section', async (t) => {
         description: 'derp',
         body: 'This is a test section'
     };
-    t.mock.method(sectionRepository, 'findSectionByTitle', async (courseCode, title) => {
-        return sectionData; // Simulate existing course with the same course code
+    t.mock.method(sectionRepository, 'isDuplicateSection', async (courseCode, title) => {
+        return sectionData.length !== 0; // Simulate existing course with the same course code
     }); 
     try {
         await sectionService.createSection(sectionData);
