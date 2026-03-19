aae6064cb9511a775950d57f15508b2666173f3d
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-19 17:18:00 -0500

    chore: change profilePic to profile_pic for consistency with other attributes in schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String, required: true, unique: true
     },
-    profilePic: {
+    profile_pic: {
         type: String,
     }
 });
