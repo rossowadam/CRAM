fd0907dfcf75d18bb7370bfb85b5f0a2dad34155
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-19 03:18:03 -0500

    refactor: make avatar constants file to export cleanly

---
+++ frontend/src/constants/avatars.ts
@@ -0,0 +1,21 @@
+import avatar1 from "../assets/avatars/avatar1.webp";
+import avatar2 from "../assets/avatars/avatar2.webp";
+import avatar3 from "../assets/avatars/avatar3.webp";
+import avatar4 from "../assets/avatars/avatar4.webp";
+import avatar5 from "../assets/avatars/avatar5.webp";
+import avatar6 from "../assets/avatars/avatar6.webp";
+import avatar7 from "../assets/avatars/avatar7.webp";
+import avatar8 from "../assets/avatars/avatar8.webp";
+import avatar9 from "../assets/avatars/avatar9.webp";
+
+export const AVATAR_MAP: Record<string, string> = {
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatar5,
+    avatar6,
+    avatar7,
+    avatar8,
+    avatar9,
+};
