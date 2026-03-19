7b37cfb29c6acd33c36ec34f84fcefa2c3de812f
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-19 18:08:21 -0500

    ui: add another pre-defined avatar

--- frontend/src/constants/avatars.ts
+++ frontend/src/constants/avatars.ts
@@ -8,6 +8,7 @@ import avatar7 from "../assets/avatars/avatar7.webp";
 import avatar8 from "../assets/avatars/avatar8.webp";
 import avatar9 from "../assets/avatars/avatar9.webp";
 import avatar10 from "../assets/avatars/avatar10.webp";
+import avatar11 from "../assets/avatars/avatar11.webp";
 
 export const AVATAR_MAP: Record<string, string> = {
     avatar1,
@@ -20,4 +21,5 @@ export const AVATAR_MAP: Record<string, string> = {
     avatar8,
     avatar9,
     avatar10,
+    avatar11,
 };
