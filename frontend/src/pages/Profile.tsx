7bc5d44ce1d5f85ac82a32eeaf0cd6334f024efd
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-21 19:11:14 -0500

    ui: add change password fields to appear when button is toggled

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -26,6 +26,10 @@ export default function Profile() {
     const [picDialogOpen, setPicDialogOpen] = useState(false);
     const [selectedPic, setSelectedPic] = useState<string | null>(null);
     const [serverError, setServerError] = useState<string | null>(null);
+    const [changePassword, setChangePassword] = useState(false);
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     // render profile change components if the profile belongs to the user
     const isOwnProfile = user?.id === userId;
@@ -159,9 +163,61 @@ export default function Profile() {
 
                 {/* Password */}
                 {isOwnProfile && 
-                <div className="flex flex-row items-center justify-center p-2 gap-5">
-                    <Button variant="outline" className=" font-medium font-funnel hover:bg-secondary hover:text-background hover:cursor-pointer">Change Password</Button>
-                </div>}
+                    <div className="flex flex-row items-center justify-center p-2 gap-5">
+                        <Button variant="outline" 
+                        className=" font-medium font-funnel hover:bg-secondary hover:text-background hover:cursor-pointer" 
+                        onClick={() => {
+                            // toggle password field and reset their inputs
+                            setChangePassword(prev => !prev)
+                            setCurrentPassword("");
+                            setNewPassword("");
+                            setConfirmPassword("");
+                        }}>
+                            Change Password
+                        </Button>
+                    </div>
+                }
+
+                {isOwnProfile && changePassword && (
+                    <div className="flex flex-col sm:p-2 gap-4 w-full">
+                        <input 
+                            type="password"
+                            placeholder="Enter current password"
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                            className="font-funnel font-thin text-xs sm:text-sm bg-background text-foreground border border-foreground rounded-md p-2 w-full"
+                        />
+                        <div>
+                            <input 
+                                type="password"
+                                placeholder="Enter new password"
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                className="font-funnel font-thin text-xs sm:text-sm bg-background text-foreground border border-foreground rounded-md p-2 w-full"
+                            />
+                            <p className={`text-xs font-funnel italic gap-0 ${newPassword.length > 0 && newPassword.length < 8 ? "text-destructive" : "text-secondary"}`}>
+                                Password must be at least 8 characters
+                            </p>
+                        </div>
+
+                        <div>
+                            <input 
+                                type="password"
+                                placeholder="Confirm new password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                className="font-funnel font-thin text-xs sm:text-sm bg-background text-foreground border border-foreground rounded-md p-2 w-full"
+                            />
+                            <p className={`text-xs font-funnel italic gap-0 ${confirmPassword.length > 0 && confirmPassword !== newPassword ? "text-destructive" : "text-secondary"}`}>
+                                Passwords must match
+                            </p>
+                        </div>
+
+                        <Button className="hover:cursor-pointer hover:text-secondary w-full">
+                            Confirm
+                        </Button>
+                    </div>
+                )}
 
                 {serverError && (
                     <p className="text-destructive text-sm text-center mt-2">
