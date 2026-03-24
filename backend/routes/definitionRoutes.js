2a1b4aa545b7d7fd2057f9e741be7babb21b1465
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-03-24 02:59:13 -0500

    chore: add require verification for CRUD operations

--- backend/routes/definitionRoutes.js
+++ backend/routes/definitionRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const definitionController = require('../controllers/definitionController');
-const { requireAuth } = require('../middleware/auth');
+const { requireAuth, requireVerification } = require('../middleware/auth');
 const{ registerSchema, validate } = require('../middleware/validators/definitionValidator');
-
 
 router.get('/:courseCode', definitionController.getDefinitionsByCourseCode);
 
-router.use(requireAuth);
+router.use(requireAuth, requireVerification);
 
-router.post('/create', registerSchema,validate, definitionController.createDefinition);
+router.post('/create', registerSchema, validate, definitionController.createDefinition);
+
 router.delete('/delete/:id', definitionController.deleteDefinition);
 
 router.put('/update/:id', registerSchema, validate, definitionController.updateDefinition);
-
 
 module.exports = router;
